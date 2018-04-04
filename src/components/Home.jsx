f05ccef46672e76d22ce03facaff5eca54b178d3
Daren Lunsford <dlunsfo1@asu.edu>
2018-04-04 14:56:59 -0500

    [Mobile] fixes bug on touch screen access

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,32 +5,36 @@ import Venues from '../container/Venues/Venues';
 import axios from 'axios';
 
 class Home extends Component {
+  mobileViewClickHandler = () => {
+    console.log('mobileViewClickHandler!');
+    window.location = '/venues';
+  }
   render(){
     return (
       <Aux>
         <div className="container-fluid">
         <div className="row">
 
-            <div className="fitted-image fit-top-center col-sm contain" style={{background: 'url(/images/plate-steak.jpg)'}}>
+            <div onClick={() => this.mobileViewClickHandler()} className="fitted-image fit-top-center col-sm contain" style={{background: 'url(/images/plate-steak.jpg)'}}>
               <div className="overlay">
               <a href="/venues"><div className="text">Steakhouses</div></a>
               </div>
           </div>
 
-            <div className="fitted-image fit-top-center col-sm contain" style={{background: 'url(/images/plate-seafood.jpg)'}}>
+            <div onClick={() => this.mobileViewClickHandler()} className="fitted-image fit-top-center col-sm contain" style={{background: 'url(/images/plate-seafood.jpg)'}}>
               <div className="overlay">
               <a href="/venues"><div className="text">Seafood Houses</div></a>
               </div>
           </div>
           </div>
           <div className="row">
-            <div className="fitted-image fit-top-center col-sm contain" style={{background: 'url(/images/steakhouse.jpg)'}}>
+            <div onClick={() => this.mobileViewClickHandler()} className="fitted-image fit-top-center col-sm contain" style={{background: 'url(/images/steakhouse.jpg)'}}>
               <div className="overlay">
               <a href="/venues"><div className="text">Texas Steakhouses</div></a>
               </div>
           </div>
 
-            <div className="fitted-image fit-top-center col-sm contain" style={{background: 'url(/images/bar-irish.jpg)'}}>
+            <div onClick={() => this.mobileViewClickHandler()} className="fitted-image fit-top-center col-sm contain" style={{background: 'url(/images/bar-irish.jpg)'}}>
               <div className="overlay">
               <a href="/venues"><div className="text">Irish Establishments</div></a>
               </div>
